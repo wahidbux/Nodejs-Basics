a13342347032ce7d8fb7cf104fbe1829740c8313
WAHID BUX K22SW <wahidbux>
2024-06-04 13:57:37 +0500

    Practice of express with api

--- src/ExpreePractice.js
+++ src/ExpreePractice.js
@@ -53,19 +53,56 @@ app.listen(5000);*/
 
 // Application level Middleware..........
 import express from 'express';
-const app=express();
+//const app=express();
 const reqFilter=((req,res,next)=>{
      console.log(reqFilter);
      next();
 })
 app.use(reqFilter);
 
-app.get('',(req,res)=>{
+app.get('',reqFilter,(req,res)=>{
     res.send("Wellcom to home page ");
 })
-app.get('/home',(req,res)=>{
+app.get('/home',reqFilter,(req,res)=>{
     res.send("This is our page ");
 })
 app.listen(5000);
 
-//Route Level middleware..........
+// Api using get ,post , put , delete methods........
+const dbConnect = require('./mongodb');
+const express = require('express');
+const mongodb = require('mongodb');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', async (res, resp) => {
+    let data = await dbConnect();
+    data = await data.find().toArray();
+    resp.send(data);
+});
+
+app.post("/", async (req, resp) => {
+    let data = await dbConnect();
+    let result = await data.insert(req.body)
+    resp.send(result)
+
+})
+
+app.put("/:name", async (req, resp) => {
+    const data = await dbConnect();
+    let result = data.updateOne(
+        { name: req.params.name },
+        { $set: req.body }
+    )
+    resp.send({ status: "updated" })
+})
+
+app.delete("/:id", async (req,resp)=>{
+    console.log(req.params.id);
+    const data = await dbConnect();
+    const result = await data.deleteOne({_id: new mongodb.ObjectId(req.params.id)})
+    resp.send(result)
+})
+
+app.listen(5000)
